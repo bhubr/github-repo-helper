7b2dd978791434cb03d4d3a34502c5c326f97c7c
Benoit Hubert <benoithubert@gmail.com>
2021-03-31 16:14:06 +0200

    Add build

---
+++ client/build/precache-manifest.da258b61dd9e5bb771738e93b1547781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9445ebc1c75c16909c3493b6262126",
+    "url": "/github-repo-helper/index.html"
+  },
+  {
+    "revision": "a6ad88cfe63301234e0b",
+    "url": "/github-repo-helper/static/css/2.1342b919.chunk.css"
+  },
+  {
+    "revision": "da9bc9ecb631afa091a3",
+    "url": "/github-repo-helper/static/css/main.e3024f20.chunk.css"
+  },
+  {
+    "revision": "a6ad88cfe63301234e0b",
+    "url": "/github-repo-helper/static/js/2.44db2d36.chunk.js"
+  },
+  {
+    "revision": "c6598a835c68722692f772a4f1c61dc6",
+    "url": "/github-repo-helper/static/js/2.44db2d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9bc9ecb631afa091a3",
+    "url": "/github-repo-helper/static/js/main.f98ba821.chunk.js"
+  },
+  {
+    "revision": "44efdd00d8eb1f1217ca",
+    "url": "/github-repo-helper/static/js/runtime-main.63ff6389.js"
+  }
+]);
